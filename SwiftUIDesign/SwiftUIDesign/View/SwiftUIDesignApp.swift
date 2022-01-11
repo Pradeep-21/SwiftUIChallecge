20635c29aee765d7c9ef585d9c0df64995f740d5
Pradeep-21 <pradeepcse21@gmail.com>
2022-01-11 22:15:32 +0530

    Change the first view

--- SwiftUIDesign/SwiftUIDesign/View/SwiftUIDesignApp.swift
+++ SwiftUIDesign/SwiftUIDesign/View/SwiftUIDesignApp.swift
@@ -13,7 +13,7 @@ struct SwiftUIDesignApp: App {
 
     var body: some Scene {
         WindowGroup {
-            SignUpView()
+            LoginView()
                 .accentColor(.stLightOrange)
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
